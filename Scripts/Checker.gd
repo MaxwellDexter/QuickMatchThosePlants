0391cc00fb9dee1889adaccbb7d9a7b8769f14cd
Unknown <ohaiguyhai@Gmail.com>
2021-10-15 23:51:55 +1100

    highlight
    still buggy oops

--- Scripts/Checker.gd
+++ Scripts/Checker.gd
@@ -2,6 +2,19 @@ extends RayCast
 
 signal clicked_object(object)
 
+var looking_at
+
+func _process(delta):
+	var col = get_collider()
+	if col != looking_at:
+		print(looking_at)
+		print(looking_at != null)
+		if looking_at != null and is_instance_valid(looking_at):
+			looking_at.targeted = false
+		looking_at = col
+		if col != null:
+			looking_at.targeted = true
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
 		if is_colliding() and get_collider().is_clickable:
--- Scripts/clickable.gd
+++ Scripts/clickable.gd
@@ -2,3 +2,14 @@ extends Node
 
 export(String) var identifier
 export(bool) var is_clickable = false
+
+onready var shader = get_parent().get_active_material(0).next_pass
+
+var targeted = false setget set_targeted
+
+func set_targeted(value):
+	targeted = value
+	if is_clickable and targeted:
+		shader.set_shader_param("strength", 0.2)
+	else:
+		shader.set_shader_param("strength", 0.0)
